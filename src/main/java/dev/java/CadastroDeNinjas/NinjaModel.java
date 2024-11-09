9eda748db7e934ae8026e17a1db392a51d3b8b8d
Vinicius <viniciusc.2005@gmail.com>
2024-11-09 10:18:01 -0300

    Cria uma entidade pelo NinjaModel

---
+++ src/main/java/dev/java/CadastroDeNinjas/NinjaModel.java
@@ -0,0 +1,54 @@
+package dev.java.CadastroDeNinjas;
+
+import jakarta.persistence.*;
+
+// transforma a class em entidade (Banco de dados)
+@Entity
+
+// criar tabela cadastro
+@Table(name = "tb_cadastro")
+
+public class NinjaModel {
+
+    // para o java saber como adicionar o id do usuario
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
+
