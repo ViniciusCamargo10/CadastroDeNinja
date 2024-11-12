cb38f6f726bc23d07490db2f44c2779d6a1c8e9c
Vinicius <viniciusc.2005@gmail.com>
2024-11-11 21:22:41 -0300

    feat: Migrations para tb_cadastro incluindo coluna de rank

--- src/main/java/dev/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
+++ src/main/java/dev/java/CadastroDeNinjas/CadastroDeNinjasApplication.java
@@ -7,10 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CadastroDeNinjasApplication {
 
 	public static void main(String[] args) {
+		// Inicializa a aplicação Spring Boot
 		SpringApplication.run(CadastroDeNinjasApplication.class, args);
 	}
-
-
-
-
 }
